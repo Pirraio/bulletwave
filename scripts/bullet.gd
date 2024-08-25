7129da83692b85d6964797d8912dc44101ce4400
Pirraio <g.teixeira7x@gmail.com>
2024-08-25 15:05:43 -0300

    Movimentação do doge melhorada e mecânica de atirar

---
+++ scripts/bullet.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed: float = 500.0
+@export var damage: int = 50
+var direction: Vector2
+
+func setup(dir: Vector2) -> void:
+	direction = dir
+
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta):
+	# Mover a bullet na direção indicada
+	position += direction * speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
