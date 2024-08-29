75b16cd86c65f3fd563b19a744533f027050d2b4
Pirraio <g.teixeira7x@gmail.com>
2024-08-28 22:52:41 -0300

    correçãdo de condicional

--- scripts/world.gd
+++ scripts/world.gd
@@ -19,7 +19,7 @@ func _ready():
 
 
 func _process(delta):
-	if Global.enemyCount >= 3:
+	if Global.enemyCount >= 10:
 		Global.enemyCount = 0
 		get_tree().change_scene_to_file("res://scenes/win.tscn")
 
